2978dd4cb04e09733362682679bae891d8ca8228
adammck <adam.mckaig@gmail.com>
2009-04-23 14:18:57 -0400

    added test for upcoming AT+CFUN after 5 errors

--- spec/rubygsm.rb
+++ spec/rubygsm.rb
@@ -20,4 +20,31 @@ describe Gsm::Modem do
 			Gsm::Modem.new(modem)
 		end.should_not raise_error
 	end
+	
+	it "resets the modem after 5 consecutive errors" do
+		
+		# this modem will return errors when AT+CSQ is
+		# called, UNTIL the modem is reset. a flag is
+		# also set, so we can check for the reset
+		class TestModem < Gsm::Mock::Modem
+			attr_reader :has_reset
+			
+			def at_csq(*args)
+				@has_reset ? super : false
+			end
+			
+			def at_cfun(*args)
+				(@has_reset = true)
+			end
+		end
+
+		# start rubygsm, and call
+		# the troublesome method
+		modem = TestModem.new		
+		gsm = Gsm::Modem.new(modem)
+		gsm.signal_strength
+		
+		# it should have called AT+CFUN!
+		modem.has_reset.should == true
+	end
 end
